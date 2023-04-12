f9c6ab1cbe6a5725e7082377b74463724be6b9e3
Monique van Benthem <m.van.benthem@amsterdam.nl>
2023-04-12 15:59:32 +0200

    "little fix"

--- features/step-definitions/launch.steps.js
+++ features/step-definitions/launch.steps.js
@@ -3,6 +3,6 @@ import { Given } from '@wdio/cucumber-framework';
 
 Given(/^I can see the homescreen$/, async () => {
     await driver.launchApp()
-    await percyScreenshot('Home', { fullPage: true, screenLengths: 8 });
     await $('//android.widget.TextView[@content-desc="Graphics"]').click()
+    await percyScreenshot('Home', { fullPage: true, screenLengths: 8 });
 });
